5a4aa575a9ba47bf13b18a9422be409ba9214fcb
Alexis Bellet <info@alexis-bellet.com>
2017-03-26 23:04:05 -0400

    feat: fetch locations from API when app launches and store them in React state

--- components/app.js
+++ components/app.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import $ from 'jquery';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			locations: []
+		}
+	}
 
   render(){
     return (
     	<div>
 	    	<h1>CHILL ZONES</h1>
-    		<Map />
+    		
     	</div>
     )
   }
 
+  // <Map />
+
+  componentDidMount() {
+  	if (this.state.locations.length === 0) {
+  		this.getLocationsFromAPI();
+  	}
+  }
+
+  getLocationsFromAPI() {
+  	$.get("/api/locations").then((data) => {
+  		let locations = data;
+  		this.setState({ locations: locations })
+  		console.log('locations were updated, here they are: ');
+  		console.log(this.state.locations);
+  	});
+  }
 }
 
 export default App;
