8dc6aee58ed08e03be1c73086270c751c39a3f64
Alexis Bellet <info@alexis-bellet.com>
2017-03-28 19:53:20 -0400

    feat: add first list of locations on app

--- components/locationsList.js
+++ components/locationsList.js
@@ -1,4 +1,25 @@
 import React from 'react';
 import styles from './infotext.scss';
+import Location from './location';
+
+class LocationsList extends React.Component {
+	render() {
+		return (
+			<div className="location-list">
+				{ this.props.locations.map((location, index) => {
+					console.log(location, index);
+					return ( 
+						<Location key={ index }
+											index={ index }
+											name={ location.name }
+											category={ location.category }
+											phone={ location.phone }
+					 />
+					)
+				}) }
+			</div>
+		)
+	}
+}
 
 export default LocationsList;
