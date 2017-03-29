99be7105dc7a248a5ec02175e2e5ba1a8220a515
Alexis Bellet <info@alexis-bellet.com>
2017-03-28 22:13:29 -0400

    feat: reset database to googleMaps format

--- api/locations/model.js
+++ api/locations/model.js
@@ -5,7 +5,7 @@ var LocationSchema = new mongoose.Schema({
    name: String,
    address: {
    	street: String,
-   	zip_code: String,
+   	postal_code: String,
    	city: String,
    	lat: Number,
    	lon: Number
