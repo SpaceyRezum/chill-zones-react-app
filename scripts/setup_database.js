44c30994f4201522cef6757e3b9b08b8e79ead5b
Alexis Bellet <info@alexis-bellet.com>
2017-03-30 20:29:55 -0400

    fix: fix database script trial 2

--- scripts/setup_database.js
+++ scripts/setup_database.js
@@ -10,18 +10,13 @@ var googleAPIkey = '&key=AIzaSyDZY5u6OOV3Xuh_EUp0sIML8maJTzakfyc'
 
 Location.remove({})
 .then(function() {
+	var previousData;
+
 	locationList.forEach(function(location) {
 
 		var url = googleAPIUrl + location.lat + ',' + location.lon + googleAPIkey;
-		var previousData;
-	
-		$.get(url, function(data) {
-			if (!data) {
-				console.log(previousData);
-				return;
-			}
 			
-			previousData = data; 
+		$.get(url, function(data) {
 
 			var l = new Location();
 			l.name = location.locationName;
@@ -34,24 +29,31 @@ Location.remove({})
 				city: '',
 				postal_code: ''
 			};
-			
+
 			var response = JSON.parse(data);
 
-			response = response.results[0];
-			
-			response.address_components.forEach(function(component) {
-				if (component.types.includes('street_number')) {
-					l.address.street = component.long_name + ' ';
-				} else if (component.types.includes('route')) {
-					l.address.street += component.long_name;
-				} else if (component.types.includes('locality')) {
-					l.address.city = component.long_name;
-				} else if (component.types.includes('postal_code')) {
-					l.address.postal_code += component.long_name;
-				}
-			});
+			if (!response) {
+				console.log(previousData);
+				return;
+			} else {
+				previousData = data;
 
-			l.save();
+				response = response.results[0];
+				
+				response.address_components.forEach(function(component) {
+					if (component.types.includes('street_number')) {
+						l.address.street = component.long_name + ' ';
+					} else if (component.types.includes('route')) {
+						l.address.street += component.long_name;
+					} else if (component.types.includes('locality')) {
+						l.address.city = component.long_name;
+					} else if (component.types.includes('postal_code')) {
+						l.address.postal_code += component.long_name;
+					}
+				});
+
+				l.save();
+			}
 		});
 	})
 })
