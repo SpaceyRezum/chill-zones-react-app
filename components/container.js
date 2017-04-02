46745b2b95ad244843d76b9b11dac6ec2ee21274
Lisa <lisafolkerson@gmail.com>
2017-04-01 21:17:40 -0400

    feat: all markers loading 🗺

--- components/container.js
+++ components/container.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoogleApiComponent from './GoogleApiComponent';
 
-import { Map, Marker } from 'google-maps-react';
+import { Map, Marker, InfoWindow } from 'google-maps-react';
 
 
 export class Container extends React.Component {
@@ -23,9 +23,20 @@ export class Container extends React.Component {
         	 zoom={14} 
         	 style={{width: '50%', height: 'calc(100% - 90px)', position: 'relative'}}
         	 initialCenter={{lat: 43.65073, lng: -79.40677}} >
+
+        	 { this.props.locations.map( (location, index) => {
+        	 	return (
+	    	 		<Marker
+	    	 			key={index}
+	    	 			position={{lat: location.address.lat, lng: location.address.lon}}
+	    	 			name={location.address.name}
+	    	 		/>
+	    	 		// console.log({location, index})
+    	 		)
+	        }) }
 	    	<Marker
-			    name={'A little left'}
-			    position={{lat: 43.65077, lng: -79.40670}} 
+			    // name={'A little left'}
+			    // position={{lat: this.props.locations[0].address.lat, lng: -79.40670}} 
 			    // icon={{
 			    //       url: "http://media.mercola.com/imageserver/public/2010/December/index-finger-12.21.jpg",
 			    //       anchor: new this.props.google.maps.Point(32,32),
